6f025fd295063c6c99fe334f8360c7a7a4de31c7
Jérémy Nadal <jnadal@idmog.com>
2021-01-18 16:50:01 +0100

    requirements etc

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
-FB_APP_ID = 779991819235845
 SECRET_KEY = ',y"xR=gCltreW`\n\rMil9,xJ}'
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL') is None :
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FB_APP_ID = 779991819235845
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = 721286305492889
